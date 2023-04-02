71708e2fa156214d63e5636a656b461bdf3bbe77
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-03 05:01:34 +0900

    엔티티 클래스 개발1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,15 +1,23 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Getter @Setter
+@Getter@Setter
 public class Member {
-    @Id
-    @GeneratedValue
-    private  Long id;
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
     private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
