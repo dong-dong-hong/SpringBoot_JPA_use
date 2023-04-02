3d11073f7aa5abc9d8926e18fcdac50ba8d8eee0
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-03 04:50:21 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -7,8 +7,17 @@ import lombok.Getter;
 @Getter
 public class Address {
 
+    protected Address(){}
+
+    public Address(String city, String stereet, String zipCode) {
+        this.city = city;
+        this.stereet = stereet;
+        this.zipCode = zipCode;
+    }
     private String city;
     private String stereet;
     private String zipCode;
+
+
 
 }
