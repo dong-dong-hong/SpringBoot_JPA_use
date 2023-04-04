e06c9f512c1572e952c356d807a4231f76aef826
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-04 17:19:04 +0900

    엔티티 설계 시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -29,7 +29,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
-    //==연관관계 메서드==//
+    //==연관관계 (편의) 메서드==//
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
