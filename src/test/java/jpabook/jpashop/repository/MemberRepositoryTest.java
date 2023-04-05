1344fa41a3115c29b9abc537f3c47767a3c7c38c
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-05 23:27:46 +0900

    회원

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -26,7 +26,7 @@ public class MemberRepositoryTest {
         member.setUsername("memberA");
         // when
         Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
+        Member findMember = memberRepository.findOne(saveId);
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
