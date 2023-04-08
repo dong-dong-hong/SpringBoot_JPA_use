777e6f8bad1c73b1ec555f87b6be7e034dbb1e2c
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-08 22:00:40 +0900

    주문 기능 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,7 +1,38 @@
 package jpabook.jpashop.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class OrderServiceTest {
+
+    @Test
+    public void productOrder() throws Exception {
+        // givem
+
+        // when
+
+        // then
+    }
+    @Test
+    public void orderCancel() throws Exception {
+        // givem
+
+        // when
+
+        // then
+    }
+    @Test
+    public void  orderCancelInventoryQuantityExceeded() throws Exception {
+        // givem
+
+        // when
+
+        // then
+    }
 
 }
