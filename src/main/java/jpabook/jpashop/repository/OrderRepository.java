6f417ed9954eb85403027c9f6e468f5dc4b40fa5
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-09 22:47:25 +0900

    검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -21,5 +22,34 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-//  public List<Order> findAll(OrderSearch orderSearch){}
+  public List<Order> findAll(OrderSearch orderSearch){
+//      return em.createQuery("select o from Order o join o.member m" +
+//                      " where o.status = :status " +
+//                      " and m.name like :name", Order.class)
+//              .setParameter("status", orderSearch.getOrderStatus())
+//              .setParameter("name", orderSearch.getMemberName())
+////              .setFirstResult(100) 시작 페이징
+//              .setMaxResults(1000) // 최대 1000건
+//              .getResultList();
+
+      String jpql = "select o from Order o join o.member m";
+      boolean isFirstCondition = true;
+
+      //주문 상태 검색
+      if (orderSearch.getOrderStatus() != null) {
+          if (isFirstCondition) {
+              jpql += " where";
+              isFirstCondition = false;
+          } else {
+              jpql += " and";
+          }
+          jpql += " o.status = :status";
+      }
+
+
+
+      return em.createQuery(jpql, Order.class) //동적쿼리 주문이든 취소든 다 들고오면
+              .setMaxResults(1000)
+              .getResultList();
+  }
 }
